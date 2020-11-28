19ab9b940920e0d16e03cd1144b7e4df8106a92c
zhongxuchen <zhongxuchen@gmail.com>
2020-11-29 00:51:20 +0800

    Update QuickVOStart.java

--- src/main/java/org/sagacity/quickvo/QuickVOStart.java
+++ src/main/java/org/sagacity/quickvo/QuickVOStart.java
@@ -26,9 +26,9 @@ public class QuickVOStart {
 	public void doStart() {
 		try {
 			out.println("\n\n=========    welcome use sagacity-quickvo-4.16.9     ==========");
-			out.println("/*----遇到问题不要慌,请关注日志提示，最常见错误有2个:                                            */\n"
-					+ "/*-1、没有匹配到表: include表达式或 schema和catalog配置错误(含大小写)          */ \n"
-					+ "/*-2、VO中字段出现重复字段，错误就是schema和catalog配置错误                            */\n"
+			out.println("/*----遇到问题不要慌,请关注日志提示，最常见错误有2个:                                            \n"
+					+ "/*-1、没有匹配到表: include表达式或 schema和catalog配置错误(含大小写)         \n"
+					+ "/*-2、VO中字段出现重复字段，错误就是schema和catalog配置错误                           \n"
 					+ "/*---------------------------------------------------------------*/\n\n ");
 			Thread.sleep(1500);
 
